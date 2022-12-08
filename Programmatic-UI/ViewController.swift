931a9d94086e90a66946fd32d0ebd72a0087b8d6
mayur.koli <mayur.koli@bookmyshow.com>
2022-12-08 13:26:31 +0530

    Initial Commit

---
+++ Programmatic-UI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Programmatic-UI
+//
+//  Created by Mayur Koli on 08/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
